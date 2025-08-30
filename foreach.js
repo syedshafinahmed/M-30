cd6c5542acb95ca5758f0164af072c1e2a57b8a7
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-31 00:38:05 +0600

    added

---
+++ foreach.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+numbers.forEach(x => console.log(x));
+numbers.forEach(x => console.log(x * 5));
+numbers.forEach(num =>{
+    const doubled = num * 2;
+    console.log(doubled);
+})
