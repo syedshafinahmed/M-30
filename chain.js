4be28224df83e0c4cbb861bdfe09337335d39bd7
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-30 23:21:59 +0600

    added

---
+++ chain.js
@@ -0,0 +1,14 @@
+const student = {
+    name: "Shafin",
+    family: {
+        title: "ahmed",
+        // father:{
+        //     name: "feroz",
+        //     age: 65
+        // }
+    },
+    1: 50,
+    "home-address": "Mirpur",
+    marks: 97
+}
+console.log(student.family.father?.age);
