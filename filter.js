9a6cf2bc30a1ef6ad82469615f093edd5d31d8c6
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-31 00:41:30 +0600

    added

---
+++ filter.js
@@ -0,0 +1,5 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const greaterThan5 = numbers.filter(x => x > 5);
+const evenNumbers = numbers.filter(y => y % 2 === 0);
+console.log(greaterThan5);
+console.log(evenNumbers);
